5cb430158fb87f6df21388b7a5c76a24ad69fec1
himuraqt <edocules123@gmail.com>
2025-02-11 00:13:50 +0700

    adding get auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,10 @@ const { signup, login } = require('../controllers/auth.js');
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.send("Auth route is working!");
+});
+
 router.post('/signup', signup);
 router.post('/login', login);
 
